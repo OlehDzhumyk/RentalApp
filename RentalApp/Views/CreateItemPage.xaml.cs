47d984d6fd1090cfad07a0324295b36bef71209d
OlehDzhumyk <olehdzhumyk+github@gmail.com>
2026-05-04 11:58:36 +0100

    feat: add CreateItemPage UI

---
+++ RentalApp/Views/CreateItemPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * @file CreateItemPage.xaml.cs
+ * @brief Code-behind for the item creation screen
+ * @author RentalApp Development Team
+ * @date 2026
+ */
+
+using RentalApp.ViewModels;
+
+namespace RentalApp.Views;
+
+/// <summary>
+/// Interaction logic for CreateItemPage.
+/// </summary>
+public partial class CreateItemPage : ContentPage
+{
+    /// <summary>
+    /// Initializes a new instance of the CreateItemPage with its ViewModel.
+    /// </summary>
+    /// <param name="viewModel">The injected CreateItemViewModel.</param>
+    public CreateItemPage(CreateItemViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}

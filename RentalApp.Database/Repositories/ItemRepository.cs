7d7fe42f6f6c312cee117f9384d35e4c08eefe88
OlehDzhumyk <olehdzhumyk+github@gmail.com>
2026-05-04 11:45:41 +0100

    feat: implement ItemRepository and register in DI container

---
+++ RentalApp.Database/Repositories/ItemRepository.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * @file ItemRepository.cs
+ * @brief EF Core implementation of IItemRepository
+ * @author RentalApp Development Team
+ * @date 2026
+ */
+
+using Microsoft.EntityFrameworkCore;
+using RentalApp.Database.Data;
+using RentalApp.Database.Models;
+
+namespace RentalApp.Database.Repositories;
+
+/// <summary>
+/// Manages Item entities using Entity Framework Core.
+/// </summary>
+public class ItemRepository : IItemRepository
+{
+    private readonly AppDbContext _context;
+
+    public ItemRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Item?> GetByIdAsync(int id)
+    {
+        return await _context.Items
+            .Include(i => i.Owner)
+            .FirstOrDefaultAsync(i => i.Id == id);
+    }
+
+    public async Task<List<Item>> GetAllAsync()
+    {
+        return await _context.Items
+            .Include(i => i.Owner)
+            .Where(i => i.IsAvailable)
+            .ToListAsync();
+    }
+
+    public async Task<List<Item>> GetByOwnerIdAsync(int ownerId)
+    {
+        return await _context.Items
+            .Where(i => i.OwnerId == ownerId)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(Item item)
+    {
+        await _context.Items.AddAsync(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Item item)
+    {
+        _context.Items.Update(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var item = await _context.Items.FindAsync(id);
+        if (item != null)
+        {
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- RentalApp/MauiProgram.cs
+++ RentalApp/MauiProgram.cs
@@ -23,6 +23,7 @@ public static class MauiProgram
 
         builder.Services.AddDbContext<AppDbContext>();
         builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IItemRepository, ItemRepository>();
         builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
         builder.Services.AddSingleton<INavigationService, NavigationService>();
 
